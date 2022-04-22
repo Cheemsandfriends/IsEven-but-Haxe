eff77bf907d5fa2e89d0fb4386e56a7e9718db30
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2022-04-22 16:43:28 +0200

    adding the one two three support shit lol

--- Main.hx
+++ Main.hx
@@ -28,8 +28,26 @@ class Main
     {
         trace("Checking if it actually works lol");
         trace("isEven 2?");
+        #if !sys
         trace(NumberChecker.isEven(2));
         trace("should be true!");
+        #else
+        trace("Oh fuck! You're checking this in interp shit!!!");
+        trace("What number do you want to check dude?");
+        var input = Sys.stdin().readLine();
+        trace(NumberChecker.isEven(input));
+        Sys.sleep(0.5);
+        trace("Was that correct?");
+        Sys.sleep(1);
+        trace("Well, how about we do isOdd?");
+        Sys.sleep(0.1);
+        trace("add your odd number!");
+        var input = Sys.stdin().readLine();
+        trace(NumberChecker.isOdd(input));
+        trace("was that correct?");
+        Sys.sleep(0.5);
+        trace("Well, anyway, bye!");
+        #end
     }
 }
 class NumberChecker
@@ -58,17 +76,121 @@ class NumberChecker
                 var lastnum = Std.parseInt(array[array.length - 1]);
                 return lastnum % 2 == 0;
             }
+            var parsedNumber = parseStringifiedNumbers(number);
+            return parsedNumber % 2 == 0;
         }
-
-        return false;
     }
     public static function isOdd(number:Number):Bool
     {
         return !isEven(number);
     }
-    private static function parseStringifiedNumbers(num:String):Number
+    private static function parseStringifiedNumbers(num:String):Float
     {
         num = StringTools.trim(num).toLowerCase();
-        return 0;
+        var numbers = StringTools.replace(num, "-", " ").split(" ");
+        var parsednumber:Float = 0;
+        var checks:Int = 0;
+        var comma:Bool = false;
+        for (number in numbers)
+        {
+            switch (number)
+            {
+                case "zero":
+                    parsednumber += 0;
+                case "a", "one":
+                    parsednumber++;
+                case "two":
+                    parsednumber += (!comma) ? 2 : 0.2;
+                case "three":
+                    parsednumber += (!comma) ? 3 : 0.3;
+                case "four":
+                    parsednumber += (!comma) ? 4 : 0.4;
+                case "five":
+                    parsednumber += (!comma) ? 5 : 0.5;
+                case "six":
+                    parsednumber += (!comma) ? 6 : 0.6;
+                case "seven":
+                    parsednumber += (!comma) ? 7 : 0.7;
+                case "eight":
+                    parsednumber += (!comma) ? 8 : 0.8;
+                case "nine":
+                    parsednumber += (!comma) ? 9 : 0.9;
+                case "ten":
+                    parsednumber += (!comma) ? 10 : 0.10;
+                case "eleven":
+                    parsednumber += (!comma) ? 11 : 0.11;
+                case "twelve":
+                    parsednumber += (!comma) ? 12 : 0.12;
+                case "thirteen":
+                    parsednumber += (!comma) ? 13 : 0.13;
+                case "fourteen":
+                    parsednumber += (!comma) ? 14 : 0.14;
+                case "fifteen":
+                    parsednumber += (!comma) ? 15 : 0.15;
+                case "sixteen":
+                    parsednumber += (!comma) ? 16 : 0.16;
+                case "seventeen":
+                    parsednumber += (!comma) ? 17 : 0.17;
+                case "eighteen":
+                    parsednumber += (!comma) ? 18 : 0.18;
+                case "nineteen":
+                    parsednumber += (!comma) ? 19 : 0.19;
+                case "twenty":
+                    parsednumber += (!comma) ? 20 : 0.20;
+                case "thirty":
+                    parsednumber += (!comma) ? 30 : 0.30;
+                case "forty", "fourty":
+                    parsednumber += (!comma) ? 40 : 0.40;
+                case "fifty":
+                    parsednumber += (!comma) ? 50 : 0.50;
+                case "sixty":
+                    parsednumber += (!comma) ? 60 : 0.60;
+                case "seventy":
+                    parsednumber += (!comma) ? 70 : 0.70;
+                case "eighty":
+                    parsednumber += (!comma) ? 80 : 0.80;
+                case "ninety":
+                    parsednumber += (!comma) ? 90 : 0.90;
+                case "hundred":
+                    parsednumber *= (!comma) ? 100 : 0.100;
+                case "thousand":
+                    parsednumber *= (!comma) ? 1000 : 0.1000;
+                case "million":
+                    parsednumber *= (!comma) ? 1000000 : 0.1000000;
+                case "billion":
+                    parsednumber *= (!comma) ? 1000000000 : 0.1000000000;
+                case "trillion":
+                    parsednumber *= (!comma) ? 1000000000 : 0.1000000000;
+                case "minus":
+                    parsednumber -= parseStringifiedNumbers(numbers[checks + 1]);
+                    numbers.splice(checks + 1, checks + 1);
+                case "plus":
+                    parsednumber += parseStringifiedNumbers(numbers[checks + 1]);
+                    numbers.splice(checks + 1, checks + 1);
+                case "multiplied":
+                    trace(numbers[checks + 1]);
+                    if (numbers[checks + 1] == "by")
+                        trace("haha, by");
+                    numbers.remove("by");
+                    parsednumber *= parseStringifiedNumbers(numbers[checks + 1]);
+                    numbers.splice(checks + 1, checks + 1);
+                case "divided":
+                    if (numbers[checks + 1] == "by")
+                    {
+                        numbers.splice(checks + 1, checks + 1);
+                    }
+                    parsednumber /= parseStringifiedNumbers(numbers[checks + 1]);
+                    numbers.splice(checks + 1, checks + 1);
+                case "", "and":
+                    continue;
+                case "comma":
+                    comma = true;
+                default:
+                    trace('What the actual fuck is $number???\n...\nDumass');
+            }
+            checks++;
+        }
+        trace(parsednumber);
+        return parsednumber;
     }
 }
